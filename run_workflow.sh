0f2f9e3c1a1993acc6819288f08b641b4ffd8cb0
ntnn19 <natannagar89@gmail.com>
2026-08-26 03:01:48 +0200

    results as the output dir

--- run_workflow.sh
+++ run_workflow.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-python -c 'import os; os.makedirs("results_/inparanoid/set1",exist_ok=True)' && snakemake -c24 --use-singularity --singularity-args '-B $(pwd)/input/inparanoid/set1:/input/ -B $(pwd)/results_/inparanoid/set1:/output/' --config set=set1 -p -w 30
-python -c 'import os; os.makedirs("results_/inparanoid/set2",exist_ok=True)' && snakemake -c24 --use-singularity --singularity-args '-B $(pwd)/input/inparanoid/set2:/input/ -B $(pwd)/results_/inparanoid/set2:/output/' --config set=set2 -p -w 30
-python -c 'import os; os.makedirs("results_/inparanoid/set3",exist_ok=True)' && snakemake -c24 --use-singularity --singularity-args '-B $(pwd)/input/inparanoid/set3:/input/ -B $(pwd)/results_/inparanoid/set3:/output/' --config set=set3 -p -w 30    
+python -c 'import os; os.makedirs("results/inparanoid/set1",exist_ok=True)' && snakemake -c24 --use-singularity --singularity-args '-B $(pwd)/input/inparanoid/set1:/input/ -B $(pwd)/results/inparanoid/set1:/output/' --config set=set1 -p -w 30
+python -c 'import os; os.makedirs("results/inparanoid/set2",exist_ok=True)' && snakemake -c24 --use-singularity --singularity-args '-B $(pwd)/input/inparanoid/set2:/input/ -B $(pwd)/results/inparanoid/set2:/output/' --config set=set2 -p -w 30
+python -c 'import os; os.makedirs("results/inparanoid/set3",exist_ok=True)' && snakemake -c24 --use-singularity --singularity-args '-B $(pwd)/input/inparanoid/set3:/input/ -B $(pwd)/results/inparanoid/set3:/output/' --config set=set3 -p -w 30
