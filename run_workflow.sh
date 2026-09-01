eba492f9886f98ac8dc9a976dd385b9304cbda33
ntnn19 <natannagar89@gmail.com>
2026-09-01 12:55:18 +0200

    updated run_workflow.sh

--- run_workflow.sh
+++ run_workflow.sh
@@ -1,4 +1,1 @@
-#!/usr/bin/env bash
-python -c 'import os; os.makedirs("results/inparanoid/set1",exist_ok=True)' && snakemake -c24 --use-singularity --singularity-args '-B $(pwd)/input/inparanoid/set1:/input/ -B $(pwd)/results/inparanoid/set1:/output/' --config set=set1 -p -w 30
-python -c 'import os; os.makedirs("results/inparanoid/set2",exist_ok=True)' && snakemake -c24 --use-singularity --singularity-args '-B $(pwd)/input/inparanoid/set2:/input/ -B $(pwd)/results/inparanoid/set2:/output/' --config set=set2 -p -w 30
-python -c 'import os; os.makedirs("results/inparanoid/set3",exist_ok=True)' && snakemake -c24 --use-singularity --singularity-args '-B $(pwd)/input/inparanoid/set3:/input/ -B $(pwd)/results/inparanoid/set3:/output/' --config set=set3 -p -w 30
+python -c 'import os; os.makedirs(results/inparanoid/set1,exist_ok=True)' && snakemake -c24 --use-singularity --singularity-args '-B /home/nadmin/repos/orthologue_mapping_snakemake/config/input/inparanoid/set1:/input/ -B /home/nadmin/repos/orthologue_mapping_snakemake/results/inparanoid/set1:/output/' --config set=set1 -p -w 30 --rerun-trigger mtime  --sdm conda apptainer
