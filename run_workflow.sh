3307396e09324b77fde3fc27d565ddbffa0078aa
ntnn19 <85831017+ntnn19@users.noreply.github.com>
2026-09-02 09:24:17 +0200

    Update run_workflow.sh

--- run_workflow.sh
+++ run_workflow.sh
@@ -1,4 +1,4 @@
 python -c 'import os; os.makedirs("results/inparanoid/set1",exist_ok=True)' && snakemake \
 -c24 --use-singularity --singularity-args \
 '-B $(pwd)/config/input/inparanoid/set1:/input/ -B $(pwd)/results/inparanoid/set1:/output/' \
---config set=set1 -p -w 60 --rerun-trigger mtime  --sdm conda apptainer -F
+--config set=set1 -p -w 60 --rerun-trigger mtime  --sdm conda apptainer
